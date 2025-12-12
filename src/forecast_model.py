5a59bc72ce0ab6434ad335df8a7ebecfd6a6549e
kseysh <kseysh123@naver.com>
2025-12-12 22:07:18 +0900

    fix: torch 버전 원상복구

--- src/forecast_model.py
+++ src/forecast_model.py
@@ -1,10 +1,7 @@
 from .models.timesfm.src.timesfm.configs import ForecastConfig
 from .models.timesfm.src.timesfm.timesfm_2p5.timesfm_2p5_torch import TimesFM_2p5_200M_torch
-import torch
 
-model = TimesFM_2p5_200M_torch.from_pretrained("google/timesfm-2.5-200m-pytorch", torch_compile=False)
-
-model.model.to(dtype=torch.float32)
+model = TimesFM_2p5_200M_torch.from_pretrained("google/timesfm-2.5-200m-pytorch", torch_compile=True)
 
 model.compile(
     ForecastConfig(
